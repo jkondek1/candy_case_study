019c8abf8ec386c441e33375cdfdbb571397b87a
Jakub Kondek <j.kondek@telekom.com>
2025-10-30 19:49:31 +0100

    feat: add download script and downloaded data

---
+++ scripts/download_data.sh
@@ -0,0 +1,15 @@
+mkdir -p data
+cd data || exit
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install wget
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get update
+    sudo apt-get install -y wget
+elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* || "$OSTYPE" == "win32"* ]]; then
+    echo "Please install wget manually or use Chocolatey: choco install wget"
+    exit 1
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+wget https://github.com/fivethirtyeight/data/blob/master/candy-power-ranking/candy-data.csv
